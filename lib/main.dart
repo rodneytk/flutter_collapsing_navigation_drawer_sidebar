1b492219d55182cd6db4e75f0c1ed23ff18e5d8c
rodneytk <rodneytk@hotmail.com>
2021-02-27 23:22:23 -0300

    Remover Debug Mode Banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Custom Navigator Drawer Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
